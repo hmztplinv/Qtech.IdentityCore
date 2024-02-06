b2c59f70fd92489b675815109f35dc8bd201884f
hmztplinv <hmz.tpal@gmail.com>
2024-02-06 16:52:17 +0300

    context add

---
+++ AspNetCoreIdentityApp.Web/Models/AppDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+}
